f414ba9647ecd6acfa9ce2dee5e08749066e9520
Stefan <oumansour.stefan@gmail.com>
2025-12-07 14:51:27 +0100

    Update Bumper.gd

--- Scripts/Bumper.gd
+++ Scripts/Bumper.gd
@@ -1,13 +1,4 @@
-extends Node3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+class_name Bumper extends Node3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is RigidBody3D:
