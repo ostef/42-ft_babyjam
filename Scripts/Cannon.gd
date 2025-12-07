1d27b3d8d860e56b48f4a1e40429ad16691f0000
Bourret Lucas <bourretlucas@gmail.com>
2025-12-07 22:38:39 +0100

    level

--- Scripts/Cannon.gd
+++ Scripts/Cannon.gd
@@ -1,13 +1,10 @@
 extends Node3D
 
-@export var cooldown_bar_position: Vector3 = Vector3(10, -50, 0)
-@onready var progress_bar = $cooldown_bar
 @onready var explosion = $Stand/Canon/ExplosionLight
 @onready var muzzle = $MuzzleFlash_1
 @onready var animation_player = $AnimationPlayer
 @onready var around = $around
-@export var cooldown_time: float = 1.5
-var current_time: float = 0.0
+@onready var cooldown = $Cooldown
 var is_on_cooldown: bool = false
 
 @export var action_name: String
@@ -15,16 +12,7 @@ var is_on_cooldown: bool = false
 @export var launch_point: Node3D
 @export var launch_velocity = 10.0
 
-func _ready():
-	#reset_state()
-	cooldown_positionning()
-
-func cooldown_positionning():
-	var active_camera = get_viewport().get_camera_3d()
-	var screen_pos = active_camera.unproject_position(self.global_position + cooldown_bar_position)
-	progress_bar.position = screen_pos - (progress_bar.size / 2)
-
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed(action_name) and not is_on_cooldown:
 		trigger_cooldown()
 		if not is_ball_around():
@@ -40,29 +28,9 @@ func _process(delta: float) -> void:
 		explosion.explosion()
 		animation_player.play("CannonShoot")
 
-	handle_cooldown(delta)
-
-func handle_cooldown(delta):
-	if is_on_cooldown:
-		update_cooldown(delta)
-
 func trigger_cooldown():
 	is_on_cooldown = true
-	current_time = cooldown_time
-	progress_bar.value = 0
-
-func update_cooldown(delta):
-	current_time -= delta
-
-	if current_time <= 0:
-		is_on_cooldown = false
-		reset_state()
-	else:
-		var ratio = 1.0 - (current_time / cooldown_time)
-		progress_bar.value = ratio * 100
-
-func reset_state():
-	progress_bar.value = 100.0
+	cooldown.start()
 
 func is_ball_around():
 	for body in around.get_overlapping_bodies():
@@ -77,4 +45,6 @@ func apply_force_around():
 			direction.y = 0
 			direction = direction.normalized()
 			body.linear_velocity = direction * 20
-	
+
+func _on_cooldown_timeout() -> void:
+	is_on_cooldown = false
