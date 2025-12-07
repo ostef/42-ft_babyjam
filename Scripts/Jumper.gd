4e0edc349c465dd5d2a5ee315db114d22952c16a
Stefan <oumansour.stefan@gmail.com>
2025-12-07 18:10:19 +0100

    Added jumper

--- Scripts/Jumper.gd
+++ Scripts/Jumper.gd
@@ -1,11 +1,8 @@
 extends Node
 
+@onready var body = $RigidBody3D
+@export var jump_force = 200.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("jumper"):
+		body.apply_torque_impulse(body.basis.x * -jump_force)
